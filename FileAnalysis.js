438b1aa62f462dc364776912df3c97b87100b010
Jeremy Sullivan <Jsullivansec95@gmail.com>
2019-02-26 13:29:08 +0000

    Updated Formatting

--- FileAnalysis.js
+++ FileAnalysis.js
@@ -1,37 +1,58 @@
-module.exports = {
+function analyseDocument(document, flaggedWords) {
+    let wordCount = 0;
+    let flaggedWordCount = 0;
+    let score = 0;
 
-    analyseDocument: function (document, flaggedWords) {
-        let wordCount = 0;
-        let flaggedWordCount = 0;
-        let score = 0;
-        let scoreRank;
+    let today = new Date();
+    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    let time = ' ' + today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    document.time_started = date + time;
 
-        let today = new Date();
-        let date = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
-        let time = ' ' + today.getHours() + ':' +  today.getMinutes() + ':' + today.getSeconds();
-        document.time_started = date+time;
+    document.words.forEach(function (docWord) {
+        wordCount = wordCount + docWord.count;
 
-        document.words.forEach(function (docWord) {
-            wordCount = wordCount + docWord.count;
+        flaggedWords.words.forEach(function (flagWord) {
+            if (docWord.word == flagWord.word) {
+                score = score + (docWord.count * flagWord.score);
+                flaggedWordCount = flaggedWordCount + docWord.count;
+            }
+        })
+    });
+    document.wordCount = wordCount;
+    document.score = score;
+    document.flaggedWordCount = flaggedWordCount;
+    document.flagPercent = (flaggedWordCount / wordCount).toPrecision(3);
+    document.status = "analysed";
 
-            flaggedWords.words.forEach(function (flagWord) {
-                if (docWord.word == flagWord.word) {
-                    score = score + (docWord.count * flagWord.score);
-                    flaggedWordCount = flaggedWordCount + docWord.count;
-                }
-            })
-        });
-        document.wordCount = wordCount;
-        document.score = score;
-        document.flaggedWordCount = flaggedWordCount;
-        document.flagPercent = (flaggedWordCount / wordCount).toPrecision(3);
-        document.status = "analysed";
-        
-        today = new Date();
-        date = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
-        time = ' ' + today.getHours() + ':' +  today.getMinutes() + ':' + today.getSeconds();
-        document.time_finished = date + time;
+    today = new Date();
+    date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    time = ' ' + today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    document.time_finished = date + time;
 
-        return document;
+    document.rank = calculateRank(document);
+
+    return document;
+}
+
+function calculateRank(document) {
+    let wordCount = document.wordCount;
+    let flaggedWordCount = document.flaggedWordCount;
+    let flagPercent = document.flagPercent;
+    let score = document.score;
+    let myEval = 0;
+    myEval = (score / flaggedWordCount) * flagPercent;
+
+    if (eval > 5) {
+        return "Maximum Priority";
+    } else if (eval > 3) {
+        return "High Priority";
+    } else if (eval > 2) {
+        return "Medium Priority";
+    } else if (eval > 1) {
+        return "Low Priority";
+    } else {
+        return "Lowest Priority";
     }
 }
+
+module.exports = { analyseDocument, calculateRank };
--- tests/FileAnalysis.test.js
+++ tests/FileAnalysis.test.js
@@ -1,99 +1,99 @@
-const fileAnalyser = require('../FileAnalysis.js');
+const fileAnalyser = require('../FileAnalysis2.js');
 var analyseFile = fileAnalyser.analyseDocument;
 
 var testDoc1 = {
-    name : "testFile",
-    filename : "122h2bab2.pdf",
-    status : "pending",
-    score : 0,
-    time_stored : "",
-    time_started : "",
-    time_finished : "",
-    ownder_id : 1,
-    flaggedWordsList : "harryPotter",
-    words : [
+    name: "testFile",
+    filename: "122h2bab2.pdf",
+    status: "pending",
+    score: 0,
+    time_stored: "",
+    time_started: "",
+    time_finished: "",
+    ownder_id: 1,
+    flaggedWordsList: "harryPotter",
+    words: [
         {
-            word : "harry",
-            count : 50
+            word: "harry",
+            count: 50
         },
         {
-            word : "wand",
-            count : 5
+            word: "wand",
+            count: 5
         },
         {
-            word : "test",
-            count : 2
+            word: "test",
+            count: 2
         },
         {
-            word : "thing",
-            count : 8
+            word: "thing",
+            count: 8
         }
     ]
 }
 
 var testDoc2 = {
-    name : "testFile",
-    filename : "122h2bab2.pdf",
-    status : "pending",
-    score : 0,
-    time_stored : "",
-    time_started : "",
-    time_finished : "",
-    ownder_id : 1,
-    flaggedWordsList : "harryPotter",
-    words : [
+    name: "testFile",
+    filename: "122h2bab2.pdf",
+    status: "pending",
+    score: 0,
+    time_stored: "",
+    time_started: "",
+    time_finished: "",
+    ownder_id: 1,
+    flaggedWordsList: "harryPotter",
+    words: [
         {
-            word : "harry",
-            count : 2
+            word: "harry",
+            count: 2
         },
         {
-            word : "reformat",
-            count : 99
+            word: "reformat",
+            count: 99
         },
         {
-            word : "me",
-            count : 2
+            word: "me",
+            count: 2
         },
         {
-            word : "daddy",
-            count : 1
+            word: "daddy",
+            count: 1
         },
         {
-            word : "wand",
-            count : 5
+            word: "wand",
+            count: 5
         },
         {
-            word : "test",
-            count : 2
+            word: "test",
+            count: 2
         },
         {
-            word : "thing",
-            count : 7
+            word: "thing",
+            count: 7
         }
     ]
 }
 
 var testFlagdWords = {
-    name : "Harry Potter",
-    words :[
+    name: "Harry Potter",
+    words: [
         {
-            word : "wand",
-            score : 2
+            word: "wand",
+            score: 2
         },
         {
-            word : "harry",
-            score : 1
+            word: "harry",
+            score: 1
         },
         {
             word: "daddy",
-            score : 3
+            score: 3
         }
     ]
 }
 
 
-describe("File Analyser",function(){
-    describe("Testing Document 1", function(){
+describe("File Analyser", function () {
+    describe("Testing Document 1", function () {
         let testReport1 = analyseFile(testDoc1, testFlagdWords);
         test("Document 1 should have a total word count of 65", () => {
             expect(testReport1.wordCount).toBe(65);
@@ -109,7 +109,7 @@ describe("File Analyser",function(){
         })
     })
 
-    describe("Testing Document 2", function(){
+    describe("Testing Document 2", function () {
         let testReport2 = analyseFile(testDoc2, testFlagdWords);
         test("Document 2 should have a total word count of 118", () => {
             expect(testReport2.wordCount).toBe(118);
